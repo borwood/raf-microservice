2efe020b2071ca5e481a1cd2bb87f982b202c403
Brennan Norwood <113669828+borwood@users.noreply.github.com>
2025-04-26 19:20:53 -0400

    Made start_without_docker for testing without building image, leaving package paths in place

---
+++ start_without_docker.py
@@ -0,0 +1,4 @@
+import app.main as main
+# Ignore me, This is just to test run from the same directory as the docker image without using the image
+if __name__ == "__main__":
+    main.app.run(debug=True)
